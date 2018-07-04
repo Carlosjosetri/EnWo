3b5908bef176e7e08b327b722055d19aeb2f8bd5
Carlosjosetri <00082217@uca.edu.sv>
2018-07-03 20:30:13 -0600

    se mueve con los clicks pero no como quiero

--- ProyectoFinalPoo/src/EnWo/EnWo.java
+++ ProyectoFinalPoo/src/EnWo/EnWo.java
@@ -5,8 +5,6 @@ public class EnWo {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+  
 
 }
