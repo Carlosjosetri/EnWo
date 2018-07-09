24851249dfa0af8e4afd6ca6afd1a55a7c3dbf30
Carlosjosetri <00082217@uca.edu.sv>
2018-07-09 01:19:32 -0600

    tabla por dao personaje y habilidad

--- ProyectoFinalPoo/src/EnWo/data/dao/PersonajesxHabilidadDAO.java
+++ ProyectoFinalPoo/src/EnWo/data/dao/PersonajesxHabilidadDAO.java
@@ -1,47 +1,51 @@
 package EnWo.data.dao;
 
-import EnWo.data.entidades.Habilidad;
-import EnWo.data.entidades.Personaje;
+
+import EnWo.data.entidades.PXH;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 
 /**
  *
  * @author Tania Orellana
  */
-public class PersonajesxHabilidadDAO extends BaseDAO<Personaje> {
+public class PersonajesxHabilidadDAO extends BaseDAO<PXH> {
 
     public PersonajesxHabilidadDAO() {
         table = new TableData("personajeXhabilidad", "_id",
                 new String[]{"PkFkpersonaje", "PkFkHabilidad"});
     }
 
- 
-
-    
     @Override
-    PreparedStatement getSelectStatement(Connection con, Personaje find, String by) {
+    PXH mapToObject(ResultSet resultSet) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    PreparedStatement getDeleteStatement(Connection con, Personaje toDelete) {
+    PreparedStatement getSelectStatement(Connection con, PXH find, String by) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    PreparedStatement getInsertStatement(Connection con, Personaje toInsert,Habilidad toInsert2) {
-               PreparedStatement ps = null;
+    @Override
+    PreparedStatement getDeleteStatement(Connection con, PXH toDelete) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    PreparedStatement getInsertStatement(Connection con, PXH toInsert) {
+                      PreparedStatement ps = null;
         String query = "INSERT INTO " + table.TABLE_NAME
                 + "(" + table.fields[0] + ", " + table.fields[1] + ") VALUES (?, ?)";
         try {
             ps = con.prepareStatement(query);
             ps.setInt(1, toInsert.getIdPersonaje());
-            ps.setInt(2, toInsert2.getIdHabilidad());
+            ps.setInt(2, toInsert.getidHabilidad());
         } catch (SQLException ex) {
             ex.printStackTrace();
             Logger.getLogger(PersonajesDAO.class.getName()).log(Level.SEVERE, null, ex);
@@ -50,17 +54,12 @@ public class PersonajesxHabilidadDAO extends BaseDAO<Personaje> {
     }
 
     @Override
-    PreparedStatement getUpdateStatement(Connection con, Personaje toUpdate) {
+    PreparedStatement getUpdateStatement(Connection con, PXH toUpdate) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    Personaje mapToObject(ResultSet resultSet) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+ 
 
-    @Override
-    PreparedStatement getInsertStatement(Connection con, Personaje toInsert) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    
+   
 }
---
+++ ProyectoFinalPoo/src/EnWo/data/entidades/PXH.java
@@ -0,0 +1,29 @@
+package EnWo.data.entidades;
+
+/**
+ *
+ * @author Tania Orellana
+ */
+public class PXH {
+
+    private int idPersonaje, idHabilidad;
+    
+    
+    public int getidHabilidad() {
+        return idHabilidad;
+    }
+
+    public void setidHabilidad(int idHabilidad) {
+        this.idHabilidad = idHabilidad;
+    }
+
+    public int getIdPersonaje() {
+        return idPersonaje;
+    }
+
+    public void setIdPersonaje(int idPersonaje) {
+        this.idPersonaje = idPersonaje;
+    }
+
+
+}
